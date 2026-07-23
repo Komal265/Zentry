b5c1f1a05e3e5cf62feac4ddd2315694a04630a6
Komal265 <komalsalunkhe891@gmail.com>
2026-07-24 02:38:32 +0530

    feat: implement Footer component with social media links and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { FaDiscord, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
+const Links = [
+    {href: 'https://github.com/Komal265', icon: <FaGithub/>},
+    {href: ' ', icon: <FaDiscord/>},
+    {href: ' ', icon: <FaTwitter/>},
+    {href: ' ', icon: <FaLinkedin/>},
+]
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-screen bg-violet-300 py-4 text-black '>
+        <div className='container mx-auto flex flex-col items-center justify-between gap-4 md:flex-row '>
+            <p className='text-center test-sm md:text-left px-4'>Komal Salunkhe</p>
+
+            <div className='flex justify-center gap-4 md:justify-start'>
+                {Links.map((link) => (
+                    <a key={link} href= {link.href} target= "_blank"
+                    rel='noopener noreferrer' className='text-black transition-colors duration-500 ease-in-out hover:text-white'>
+                        {link.icon}
+                    </a>
+                ))}
+            </div>
+            <div className='text-center text-sm  hover:underline md:text-right '>ksalunkhe830@gmail.com</div>
+            
+        </div>
+    </footer>
   )
 }
 
